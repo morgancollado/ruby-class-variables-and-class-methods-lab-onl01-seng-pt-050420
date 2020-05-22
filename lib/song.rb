31b8f78361b609a7d04c2541877144fcb9754ac6
Morgan Collado <morgan.collado@gmail.com>
2020-05-22 23:18:50 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist
+  end 
+  
+  def self.count
+    @@count
+  end 
+ 
+ def self.genres
+   @@genres.uniq
+ end 
+ 
+ def self.artists
+   @@artists.uniq
+ end 
+ 
+ def self.genre_count
+   new_hash = {} 
+   @@genres.collect do |genre|
+     new_hash[genre] = @@genres.count(genre)
+   end 
+   new_hash
+ end 
+
+def self.artist_count
+  artist_hash = {}
+  @@artists.collect do |artist|
+    artist_hash[artist] = @@artists.count(artist)
+  end 
+  artist_hash
+end 
+
+
+end 
